00c5ed19bb69257043c67d64b3df128bbfcb84a2
Jinator <144614487+Jinator@users.noreply.github.com>
2023-11-19 20:54:12 -0600

    Update requestUnicorn.js

--- js/requestUnicorn.js
+++ js/requestUnicorn.js
@@ -9,27 +9,27 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 const fleet = [
 	{
-		Name: 'Bucephalus',
+		Name: 'John Whitgrove 1',
 		Color: 'Golden',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Scrappy',
+		Name: 'John Whitgrove 2',
 		Color: 'Green',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Cosmo',
+		Name: 'John Whitgrove 3',
 		Color: 'RoyalBlue',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Shadowfax',
+		Name: 'John Whitgrove 4',
 		Color: 'White',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Rocinante',
+		Name: 'John Whitgrove 5',
 		Color: 'Yellow',
 		Gender: 'Female',
 	},
